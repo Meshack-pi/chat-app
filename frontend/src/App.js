683cdd3b79f098dbf954419a4855d4b9ea265c6d
Meshack-pi <kipronomeshack36@gmail.com>
2022-06-02 17:23:44 +0000

    Modified files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element = { <Home />} />
+        <Route path="/" element = { <Home />} />
+        <Route path="/login" element = { <Home />} />
+        <Route path="signup/" element = { <Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
